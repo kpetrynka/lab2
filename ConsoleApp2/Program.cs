5f0c0b1c081dee97d91226b9789db53fed5542a0
Khrystyna Petrynka <kpetrynka@kse.org.ua>
2023-03-15 12:20:25 +0200

    trying to fix

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -10,15 +10,16 @@ var generator = new MapGenerator(new MapGeneratorOptions()
 var map = generator.Generate();
 var start = new Point(43, 12); 
 var target = new Point(26, 27);
-
+var dots = new List<Point> {start, target};
 // origins the way of getting to the point
 // distance - the distance
-new MapPrinter().Print(map, GetShortestPath(map, start, target ));
- 
+// new MapPrinter().Print(map, GetShortestPath(map, start, target ));
+new MapPrinter().Print(map, dots);
+
 
 List<Point> GetShortestPath(string[,] maze, Point starter, Point goal)
 {
-    var path = new List<Point> {starter, goal}; 
+    var path = new List<Point> {starter}; 
     var open = new List<Point>();
     var closed = new List<Point>();
     
@@ -27,9 +28,9 @@ List<Point> GetShortestPath(string[,] maze, Point starter, Point goal)
     while (open.Count > 0)
     {
         // Sort the open list by f-cost and select the first (i.e., lowest f-cost) point
-    	// The whole algorithm not the Dijkstra's 
         var current = open.MinBy(p => FCost(p, start, target));
-        Console.WriteLine(FCost(current, start, target));
+        path.Add(current);
+        // Console.WriteLine(FCost(current, start, target));
         
         
         if (current.Equals(goal))
@@ -43,13 +44,13 @@ List<Point> GetShortestPath(string[,] maze, Point starter, Point goal)
         closed.Add(current);
 
         var neighbours = GetNeighbours(current.Column, current.Row, maze);
-        Console.WriteLine(neighbours.Count);
+        // Console.WriteLine(neighbours.Count);
         foreach (var neighbour in neighbours)
         {
             if (!closed.Contains(neighbour))
             {
                 open.Add(neighbour);
-                maze[neighbour.Column, neighbour.Row] = ".";
+                // maze[neighbour.Column, neighbour.Row] = ".";
             }
         }
     }
@@ -105,46 +106,38 @@ string CheckPosition(Point point, string[,] map)
 }
 
 // Check if cell is walkable
-bool IsTraversable(Point point, string[,] map)
-{
-    if (map[point.Row, point.Column] != MapGenerator.Wall)
-    {
-        return true;
-    }
-
-    return false;
+bool IsTraversable(Point point, string[,] maze)
+{ 
+    return maze[point.Row, point.Column] != MapGenerator.Wall;
 }
 
 
-
-
-
-double HCost(Point current, Point target)
+double HCost(Point current, Point final)
 {
     // The distance from the current point to the target one (B)
-    int dx = target.Column - current.Column;
-    int dy = target.Row - current.Row;
+    int dx = final.Column - current.Column;
+    int dy = final.Row - current.Row;
     double distance = Math.Sqrt(dx*dx + dy*dy);
    
     return distance;
 
 }
 
-double GCost(Point start, Point current)
+double GCost(Point begin, Point current)
 {
     
     // The distance from the starting point (A) to the current one
-    int dx = current.Column - start.Column;
-    int dy = current.Row - start.Row;
+    int dx = current.Column - begin.Column;
+    int dy = current.Row - begin.Row;
     double distance = Math.Sqrt(dx*dx + dy*dy);
 
     return distance;
 }
 
-double FCost(Point current, Point start, Point target)
+double FCost(Point current, Point begin, Point final)
 {
-    double gCost = GCost(start, current);
-    double hCost = HCost(current, target);
+    double gCost = GCost(begin, current);
+    double hCost = HCost(current, final);
     double fCost = gCost + hCost;
 
     return fCost;
